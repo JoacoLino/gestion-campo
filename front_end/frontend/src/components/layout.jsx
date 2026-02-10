892163d56b4dbad2d28d8ae73f2c827c87793456
Joaquin Lino <joaquin.linoalonso@gmail.com>
2026-02-09 23:21:34 -0300

    s

--- front_end/frontend/src/components/layout.jsx
+++ front_end/frontend/src/components/layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
-import './Layout.css';
+import './layout.css';
 
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
